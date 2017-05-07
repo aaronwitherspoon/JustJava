93640074589eebe8d9e2da0b4175dce11381d611
aronzer <a.witherspoon@gmail.com>
2017-05-07 08:41:47 -0500

    added order summary

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -12,8 +12,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import java.text.NumberFormat;
-
 /**
  * This app displays an order form to order coffee.
  */
@@ -31,24 +29,30 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(quantity);
-        displayPrice(quantity*5);
+        int price = calculatePrice();
+        createOrderSummary(price);
+    }
+
+    public void createOrderSummary(int price) {
+        String priceMessage = "Name: Aaron";
+        priceMessage = priceMessage + "\nQuantity: " + quantity + "\nTotal: $" + price + "\nThank you!";
+        displayMessage(priceMessage);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int numberOfCoffees) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + numberOfCoffees);
     }
 
     /**
-     * This method displays the given price on the screen.
+     * Calculates the price of the order.
+     *
      */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    private int calculatePrice() {
+        return quantity * 5;
     }
 
     /**
@@ -56,8 +60,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
         quantity = quantity + 1;
-        display(quantity);
-        displayPrice(quantity * 5);
+        displayQuantity(quantity);
     }
 
     /**
@@ -65,8 +68,15 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrement(View view) {
         quantity = quantity - 1;
-        display(quantity);
-        displayPrice(quantity * 5);
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
 }
